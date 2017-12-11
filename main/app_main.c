e0695a3c58dcc5dcf3e112354af83a092f4701b7
David Antliff <david@pitchblende.co.nz>
2017-12-11 22:32:36 +1300

    Full strip operational at 10MHz.

--- main/app_main.c
+++ main/app_main.c
@@ -26,6 +26,61 @@ void led_task(void * pvParameter)
 	}
 }
 
+void led_start(spi_device_handle_t handle)
+{
+    uint8_t data[4] = { 0, 0, 0, 0 };
+    spi_transaction_t trans_desc = {
+        .address = 0,
+        .command = 0,
+        .flags = 0,
+        .length = 4 * 8,  // bits
+        .rxlength = 0,
+        .tx_buffer = data,
+        .rx_buffer = data,
+    };
+
+    ESP_LOGD(TAG, "... Transmitting.");
+    ESP_ERROR_CHECK(spi_device_transmit(handle, &trans_desc));
+}
+
+void led_end(spi_device_handle_t handle, int num_leds)
+{
+    int num_end_bits = (num_leds + 1) / 2;
+    int num_end_bytes = (num_end_bits + 7) / 8;
+
+    // for now, just send four zero bytes
+    uint8_t data[4] = { 0, 0, 0, 0 };
+    spi_transaction_t trans_desc = {
+        .address = 0,
+        .command = 0,
+        .flags = 0,
+        .length = 4 * 8,  // bits
+        .rxlength = 0,
+        .tx_buffer = data,
+        .rx_buffer = data,
+    };
+
+    ESP_LOGD(TAG, "... Transmitting.");
+    ESP_ERROR_CHECK(spi_device_transmit(handle, &trans_desc));
+}
+
+void led_set(spi_device_handle_t handle, uint8_t brightness, uint8_t red, uint8_t green, uint8_t blue)
+{
+    uint8_t data[4] = { 0xe0 + brightness, blue, green, red };
+    spi_transaction_t trans_desc = {
+        .address = 0,
+        .command = 0,
+        .flags = 0,
+        .length = 4 * 8,  // bits
+        .rxlength = 0,
+        .tx_buffer = data,
+        .rx_buffer = data,
+    };
+
+    ESP_LOGD(TAG, "... Transmitting.");
+    ESP_ERROR_CHECK(spi_device_transmit(handle, &trans_desc));
+}
+
 void test_spi_task(void * pvParameter)
 {
     spi_bus_config_t bus_config = {
@@ -46,7 +101,7 @@ void test_spi_task(void * pvParameter)
         .duty_cycle_pos = 0,
         .cs_ena_posttrans = 0,
         .cs_ena_pretrans = 0,
-        .clock_speed_hz = 10000,
+        .clock_speed_hz = 10000000,
         .spics_io_num = PIN_LED_CS, //-1,
         .flags = 0,
         .queue_size = 1,
@@ -55,23 +110,35 @@ void test_spi_task(void * pvParameter)
     };
     ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &dev_config, &handle));
 
-    uint8_t data[3] = { 0 };
-    spi_transaction_t trans_desc = {
-        .address = 0,
-        .command = 0,
-        .flags = 0,
-        .length = 3 * 8,  // bits
-        .rxlength = 0,
-        .tx_buffer = data,
-        .rx_buffer = data,
-    };
+//    led_set(handle, 0x0f, 0xff, 0x00, 0x00);
+//    led_set(handle, 0x0f, 0xff, 0x3f, 0x00);
+//    led_set(handle, 0x0f, 0xff, 0xff, 0x00);
+//    led_set(handle, 0x0f, 0x00, 0xff, 0x00);
+//    led_set(handle, 0x0f, 0x00, 0x00, 0xff);
+//    led_set(handle, 0x0f, 0xff, 0x00, 0xff);
 
-    data[0] = 0x12;
-    data[1] = 0x34;
-    data[2] = 0x56;
+    int num_leds = 60;
+    uint8_t brightness = 0x01;
+    uint8_t red = 0xff;
+    uint8_t green = 0x00;
+    uint8_t blue = 0x00;
 
-    ESP_LOGI(TAG, "... Transmitting.");
-    ESP_ERROR_CHECK(spi_device_transmit(handle, &trans_desc));
+    while (1)
+    {
+        led_start(handle);
+        for (int i = 0; i < num_leds; ++i)
+        {
+            led_set(handle, brightness, red, green, blue);
+        }
+        led_end(handle, num_leds);
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        uint8_t tmp = blue;
+        blue = green;
+        green = red;
+        red = tmp;
+    }
+
 
     ESP_LOGI(TAG, "... Removing device.");
     ESP_ERROR_CHECK(spi_bus_remove_device(handle));
